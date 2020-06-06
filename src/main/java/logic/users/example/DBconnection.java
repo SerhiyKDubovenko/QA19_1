3948c5f8a28c212091af36774fe0457b32aa2262
SerhiyKDubovenko <valenok1989@gmail.com>
2020-06-06 10:23:18 +0300

    commit 06.06.2020 master commit after merge

--- src/main/java/logic/users/example/DBconnection.java
+++ src/main/java/logic/users/example/DBconnection.java
@@ -4,5 +4,8 @@ public class DBconnection {
 
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
     }
 }

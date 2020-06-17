0183306b1c955bf558e5bcd41896da5a816c2668
Kucher88 <irina.kucher88@gmail.com>
2020-06-17 21:25:22 +0300

    added file manager, user config and connection

--- src/main/java/logic/users/realEstate_db/Connection.java
+++ src/main/java/logic/users/realEstate_db/Connection.java
@@ -1,4 +1,36 @@
 package logic.users.realEstate_db;
 
+import com.google.gson.Gson;
+import logic.users.example.UserConfig;
+import logic.users.example.common.FileManager;
+
+import java.io.File;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+
+    private static java.sql.Connection connection;
+
+    private static String getConnectionUrl(String host, String dbPort, String dbName){
+        return "jdbc:mysql://"+host+":"+dbPort+"/"+dbName;
+    }
+
+    public static java.sql.Connection getConnection(File file){
+
+        if(connection==null){
+            UserConfig config = new Gson().fromJson(FileManager.readFile(file), UserConfig.class);
+            try {
+                connection =  DriverManager
+                        .getConnection(getConnectionUrl(config.getHost(), config.getPort(), config.getDb()),
+                                config.getUser(), config.getPassword());
+            } catch (SQLException sqlex) {
+                sqlex.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+
+
 }

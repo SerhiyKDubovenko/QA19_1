d653f67868d71294c9c4aa5577e3906bed756f49
chernomorets-dev <chernomorets.dev@gmail.com>
2020-06-19 20:36:22 +0300

    asasdasd

--- src/main/java/logic/users/db_oc/CPUs.java
+++ src/main/java/logic/users/db_oc/CPUs.java
@@ -1,4 +1,54 @@
 package logic.users.db_oc;
 
+import lombok.Data;
+
+@Data
 public class CPUs {
+
+    private int CpuID;
+    private int Cores;
+    private int BrandKey;
+    private int ModelKey;
+
+    public CPUs() {
+    }
+
+    public CPUs(int cpuID, int cores, int brandKey, int modelKey) {
+        CpuID = cpuID;
+        Cores = cores;
+        BrandKey = brandKey;
+        ModelKey = modelKey;
+    }
+
+    public int getCpuID() {
+        return CpuID;
+    }
+
+    public void setCpuID(int cpuID) {
+        CpuID = cpuID;
+    }
+
+    public int getCores() {
+        return Cores;
+    }
+
+    public void setCores(int cores) {
+        Cores = cores;
+    }
+
+    public int getBrandKey() {
+        return BrandKey;
+    }
+
+    public void setBrandKey(int brandKey) {
+        BrandKey = brandKey;
+    }
+
+    public int getModelKey() {
+        return ModelKey;
+    }
+
+    public void setModelKey(int modelKey) {
+        ModelKey = modelKey;
+    }
 }
--- src/main/java/logic/users/db_oc/Models.java
+++ src/main/java/logic/users/db_oc/Models.java
@@ -1,4 +1,13 @@
 package logic.users.db_oc;
 
+import lombok.Data;
+
+@Data
 public class Models {
+
+    private int ModelsID;
+    private String Model;
+    private String Aliases;
+    private int BrandKey;
+
 }
